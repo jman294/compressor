1319bd39d962a414c2d44facd3578dfdba0e1014
Jack DuPlessis <jduplessis294@gmail.com>
2018-10-01 17:26:48 +0000

    Add model header

---
+++ src/impl/model.c
@@ -0,0 +1,9 @@
+#include "model.h"
+
+void MO_New (Model * m) {
+  m->predictions = int[65336];
+}
+
+int MO_GetPrediction (Model * m, int context) {
+  return m->predictions[context];
+}
---
+++ src/include/packingtape/model.h
@@ -0,0 +1,12 @@
+#ifndef MODEL_H_   /* Include guard */
+#define MODEL_H_
+
+typedef struct Model {
+  int[] predictions;
+} Model;
+
+void MO_New (Model * m);
+
+int MO_GetPrediction (Model * m);
+
+#endif // MODEL_H_
