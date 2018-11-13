9a1b2c908b2a0b941849adf9be904978c4eab466
Jack DuPlessis <jduplessis294@gmail.com>
2018-11-12 22:37:20 -0500

    Having trouble with meson

--- src/models/simple.c
+++ src/models/simple.c
@@ -1,15 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int main (int argc, char ** argv) {
-  FILE* =
   if (argc!=3) {
-    printf("Usage: {NAME} {INPUT_FILE}");
+    printf("Usage: {NAME} {INPUT_FILE}\n");
     exit(1);
   }
 
   // Open files
   FILE *input = fopen(argv[2], "wb");
-  if (!output) perror(argv[3]), exit(1);
+  if (!input) perror(argv[2]), exit(1);
 
   int c;
+  printf("Hello!\n");
   while((c=getc(input))!=EOF) {
     printf("%d", c);
   }
