e5066a9e93b01f1cb95a54230fa905594a0cca40
Jack DuPlessis <jduplessis294@gmail.com>
2018-11-17 22:46:43 -0500

    Give up on good model structure. Meson is impossible

--- src/impl/models/simple.c
+++ src/impl/models/simple.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 
 int main (int argc, char ** argv) {
-  if (argc!=3) {
+  if (argc != 3) {
     printf("Usage: {NAME} {INPUT_FILE}\n");
     exit(1);
   }
 
   // Open files
-  FILE *input = fopen(argv[2], "wb");
+  FILE *input = fopen(argv[2], "rb");
   if (!input) perror(argv[2]), exit(1);
 
   int c;
   printf("ssso!\n");
+  printf("%d\n", getc(input));
   while((c=getc(input))!=EOF) {
-    printf("%d", c);
+    printf("%d\n", c);
   }
 }
